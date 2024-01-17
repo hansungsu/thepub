2ca09a04dbf99148cb8dd200944a75cd47f5951d
HanSungSoo <thepub03@gmail.com>
2024-01-17 21:34:30 +0900

    [game] setting init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
+"use client";
 import { Container } from "@/components/container";
+import { useEffect } from "react";
+import Example from "@/components/game/Example";
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: "phaser-container",
+  scene: Example,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 },
+    },
+  },
+};
 
 export default function Home() {
+  useEffect(() => {
+    const game = new Phaser.Game(config);
+    return () => {
+      game.destroy(true);
+    };
+  }, []);
   return (
     <div>
-      <Container>main page hi !</Container>
+      <Container>방향키를 찌끄리면 코끼리를 움직일 수 있습니다.</Container>
+      <div
+        id="phaser-container"
+        className="flex items-center justify-center"
+      ></div>
     </div>
   );
 }
---
+++ src/components/game/example.jsx
@@ -0,0 +1,40 @@
+import Phaser from "phaser";
+
+export default class Example extends Phaser.Scene {
+  downKeyDebug;
+  upKeyDebug;
+  rightKeyDebug;
+  leftKeyDebug;
+  player;
+  cursors;
+
+  preload() {
+    this.load.setBaseURL("https://labs.phaser.io");
+    this.load.image("sky", "assets/skies/space3.png");
+    this.load.image("elephant", "assets/sprites/elephant.png");
+  }
+
+  create() {
+    this.add.image(400, 300, "sky");
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.player = this.physics.add.image(400, 300, "elephant");
+
+    this.player.setCollideWorldBounds(true);
+  }
+  update() {
+    this.player.setVelocity(0);
+
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-300);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(300);
+    }
+
+    if (this.cursors.up.isDown) {
+      this.player.setVelocityY(-300);
+    } else if (this.cursors.down.isDown) {
+      this.player.setVelocityY(300);
+    }
+  }
+}
