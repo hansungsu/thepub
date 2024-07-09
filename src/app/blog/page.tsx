da912048dfc1fd9f1f0b5f445ee2f2144a8591dc
HOTSAUCE <38658225+hansungsu@users.noreply.github.com>
2024-07-09 08:51:16 +0000

    nav fit

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -19,7 +19,7 @@ export default async function Blog() {
   return (
     <div>
       <BlogList />
-      <ul>
+      {/* <ul>
         {posts.map((item: any) => (
           <li key={item.id}>
             <Link href={`/blog/${item.id}`}>
@@ -27,7 +27,7 @@ export default async function Blog() {
             </Link>
           </li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -15,8 +15,8 @@ import { Input } from "@/components/ui/input";
 
 export default function Nav() {
   return (
-    <nav>
-      <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+    <nav className="container max-w-screen-xl">
+      <header className="sticky top-0 flex h-16 items-center gap-4 bg-background px-4">
         <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
           <Link
             href="/"
