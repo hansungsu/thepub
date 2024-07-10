c9781c848848cbb651dbff0732dda00005ec0a0d
HOTSAUCE <38658225+hansungsu@users.noreply.github.com>
2024-07-10 00:59:39 +0000

    eslint modify

--- src/components/blog/BlogList.tsx
+++ src/components/blog/BlogList.tsx
@@ -92,6 +92,7 @@ export function BlogList() {
             have. Looking forward to
           </div>
           <div className="flex items-center gap-2">
+            <Badge>meeting</Badge>
             <div className="inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80">
               meeting
             </div>
